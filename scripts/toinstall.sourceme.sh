aaf167d93563e7f0d65d3c66f565f763af6d9997
Jérémy Fanguède <jeremy.fanguede@gmail.com>
2020-02-06 15:44:47 +0100

    scripts: Add helper install script

---
+++ scripts/toinstall.sourceme.sh
@@ -0,0 +1,7 @@
+sudo apt install -y build-essential git protobuf-compiler
+sudo apt install -y python3-pip python3-pandas cython3 python3-opencv
+
+pip3 install tensorflow
+#pip3 install tensorflow-gpu
+
+pip3 install pycocotools
